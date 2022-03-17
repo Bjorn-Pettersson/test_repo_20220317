feaaef5c88a26a114bc705671591195e6bcf468a
Bjorn-Pettersson <54637980+Bjorn-Pettersson@users.noreply.github.com>
2022-03-17 14:15:05 +0100

    added print "more stuff" to shape class printstuff function

---
+++ Shape.py
@@ -0,0 +1,10 @@
+class Shape():
+    def __init__(self) -> None:
+        pass
+    
+    def printStuff():
+        print("stuff")
+        print("more stuff")
+
+if __name__ == "__main__":
+    pass
